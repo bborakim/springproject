dc76429e0f047c253ca0c3e1e36b713f4ee98248
kimbora <kimbora@192.168.0.93>
2018-01-22 19:49:03 +0900

    first commit

---
+++ ex00/src/main/java/org/zerock/web/SampleController5.java
@@ -0,0 +1,16 @@
+package org.zerock.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.zerock.domain.ProductVo;
+
+@Controller
+public class SampleController5 {
+
+	@RequestMapping("/doJSON")
+	public @ResponseBody ProductVo doJSON() {
+		ProductVo vo = new ProductVo("샘플상품", 300000);
+		return vo;
+	}
+}
